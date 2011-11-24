226f47405d578cf45cf7ebd2b8ecceacbee157f3
Aaron Powell <me@aaron-powell.com>
2011-11-24 23:01:28 +0000

    adding more random util tests

--- tests/util-random-spec.js
+++ tests/util-random-spec.js
@@ -20,4 +20,12 @@ describe('tbd-util-random', function() {
                     
         expect(data[0].foo).toBeInArray([1,2,3,4,5]);
     });
+    
+    it('should randomly choose from string arguments', function() {
+        var data = tbd.from({ foo: '' })
+                    .prop('foo').use(tbd.utils.random('foo', 'bar', 'baz')).done()
+                    .make(1);
+                    
+        expect(data[0].foo).toBeInArray(['foo', 'bar', 'baz']);
+    });
 });
