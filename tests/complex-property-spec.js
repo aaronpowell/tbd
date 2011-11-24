761ac70d35805b79e59f38b8d9e90746b49332db
Aaron Powell <me@aaron-powell.com>
2011-11-24 11:25:35 +0000

    complex object basic test

---
+++ tests/complex-property-spec.js
@@ -0,0 +1,12 @@
+describe('tbd-complex-properties', function() {
+    var tbd = require('../lib/');
+    it('should clone complex properties', function() {
+        var data = tbd.from({
+                        foo: {
+                            bar: 'baz'
+                        }
+                    }).make(2);
+
+        expect(data[0].foo).not.toBe(data[1].foo);
+    });
+});
