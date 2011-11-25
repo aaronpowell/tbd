7065fab0081b42c0c3ec669aa01cdc9090bf509a
Aaron Powell <me@aaron-powell.com>
2011-11-25 05:58:40 +0000

    supporting custom constructor arguments

--- tests/objects-from-ctor-spec.js
+++ tests/objects-from-ctor-spec.js
@@ -16,4 +16,18 @@ describe('tbd-supports-ctor-pattern', function() {
         var data = tbd.from(c).make(2);
         expect(data[0]).not.toBe(data[1]);
     });
+    
+    it('should allow custom ctor arguments', function() {
+        var c = function(a, b) {
+            this.a = a;
+            this.b = b;
+        };
+        
+        var data = tbd.from(c)
+                    .constructWith('a', 'b')
+                    .make(1);
+        
+        expect(data[0].a).toEqual('a');
+        expect(data[0].b).toEqual('b');
+    });
 });
