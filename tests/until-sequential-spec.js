d031cad683b7ddea3be3b7a69598f12c9c7004c9
Aaron Powell <me@aaron-powell.com>
2011-12-04 06:34:52 +0000

    sequential letters

--- tests/until-sequential-spec.js
+++ tests/until-sequential-spec.js
@@ -1,12 +1,21 @@
 describe('tbd-util-sequantial', function() {
     var tbd = require('../lib/');
     
-    it('should generate a sequential value for each record', function() {
+    it('should generate a sequential value for each record', function () {
         var data = tbd.from({})
                     .prop('foo').use(tbd.utils.sequential(0))
                     .make(2);
                     
         expect(data[0].foo).toBe(0);
         expect(data[1].foo).toBe(1);
+    });
+    
+    it('should handle letters', function () {
+        var data = tbd.from({})
+                    .prop('foo').use(tbd.utils.sequential('a'))
+                    .make(2);
+                    
+        expect(data[0].foo).toBe('a');
+        expect(data[1].foo).toBe('b');
     });
 });
