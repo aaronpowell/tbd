78f623e115ad4e62754e667c27eaef0ef033ac4d
Aaron Powell <me@aaron-powell.com>
2011-12-13 23:00:45 +0000

    adding tests for functions in object literals

--- tests/basic-api-spec.js
+++ tests/basic-api-spec.js
@@ -1,5 +1,22 @@
 describe('tbd-builder', function() {
     var tbd = require('../lib/');
+    
+    beforeEach(function() {
+        this.addMatchers({
+            toBeInArray: function(array) {
+                return ~array.indexOf(this.actual);
+            },
+            toBeInDateRange: function(min, max) {
+                var actual = this.actual.getTime();
+                return actual <= max.getTime() && actual >= min.getTime();
+            },
+            toBeInNumericalRange: function (min, max) {
+                var actual = this.actual;
+                return actual <= max && actual >= min;
+            }
+        });
+    });
+    
     it('should allow usage of undefined', function() {
         var amount = 10,
             data = tbd.from().make(amount);
@@ -20,5 +37,22 @@ describe('tbd-builder', function() {
             
         expect(data.length).toEqual(number);
         expect(data.length).toEqual(data2.length);
+    });
+    
+    it('should allow functions in the object literal', function () {
+        var data = tbd.from({
+            a: 1,
+            b: function () { return 'hello'; },
+            c: tbd.utils.range(1, 10),
+            d: tbd.utils.random('a', 'b', 'c'),
+            e: tbd.utils.sequential(1)
+        }).make(1);
+        
+        expect(data.length).toEqual(1);
+        expect(data[0].a).toEqual(1);
+        expect(data[0].b).toEqual('hello');
+        expect(data[0].c).toBeInNumericalRange(1, 10);
+        expect(data[0].d).toBeInArray('a', 'b', 'c');
+        expect(data[0].e).toEqual(1);
     });
 });
